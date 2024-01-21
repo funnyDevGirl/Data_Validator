57447524dfa15898149dbab44a62ff6006136a61
alina tarasova <alishka.tarasova@gmail.com>
2024-01-21 20:57:28 +0300

    Added MapSchema.java

---
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -0,0 +1,26 @@
+package hexlet.code.schemas;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class MapSchema extends BaseSchema {
+
+    //
+    public MapSchema(){
+        addCondition("type",
+                value -> value instanceof Map || value == null);
+    }
+
+    //
+    public MapSchema required() {
+        addCondition("required",
+                Objects::nonNull);
+        return this;
+    }
+
+    public MapSchema sizeof(int size) {
+        addCondition("size",
+                value -> ((Map) value).size() == size);
+        return this;
+    }
+}
