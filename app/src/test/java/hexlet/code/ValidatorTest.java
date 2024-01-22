31d632612731ee12a805639aa2188a7097828742
alina tarasova <alishka.tarasova@gmail.com>
2024-01-22 11:53:49 +0300

    Updated ValidatorTest.java

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -34,7 +35,7 @@ class ValidatorTest {
         assertThat(schema.contains("whatthe").isValid("what does the fox say")).isFalse();
 
         assertThat(schema.isValid("what does the fox say")).isFalse();
-// Здесь уже false, так как добавлена еще одна проверка contains("whatthe")
+        // Здесь уже false, так как добавлена еще одна проверка contains("whatthe")
     }
 
     @Test
@@ -43,7 +44,7 @@ class ValidatorTest {
 
         NumberSchema schema = v.number();
 
-// Пока не вызван метод required(), null считается валидным
+        // Пока не вызван метод required(), null считается валидным
         assertThat(schema.isValid(null)).isTrue();
         assertThat(schema.positive().isValid(null)).isTrue();
 
@@ -53,9 +54,9 @@ class ValidatorTest {
         assertThat(schema.isValid("5")).isFalse();
         assertThat(schema.isValid(10)).isTrue();
 
-// Потому что ранее мы вызвали метод positive()
+        // Потому что ранее мы вызвали метод positive()
         assertThat(schema.isValid(-10)).isFalse();
-//  Ноль — не положительное число
+        //  Ноль — не положительное число
         assertThat(schema.isValid(0)).isFalse();
 
         schema.range(5, 10);
@@ -87,5 +88,49 @@ class ValidatorTest {
         assertThat(schema.isValid(data)).isFalse();
         data.put("key2", "value2");
         assertThat(schema.isValid(data)).isTrue();
+    }
+
+    @Test
+    public void testMapWithShapeValidator() {
+        Validator v = new Validator();
+
+        MapSchema schema = v.map();
+
+        // shape позволяет описывать валидацию для значений каждого ключа объекта Map
+        // Создаем набор схем для проверки каждого ключа проверяемого объекта
+        // Для значения каждого ключа - своя схема
+        Map<String, BaseSchema> schemas = new HashMap<>();
+
+        // Определяем схемы валидации для значений свойств "name" и "age"
+        // Имя должно быть строкой, обязательно для заполнения
+        schemas.put("name", v.string().required());
+        // Возраст должен быть положительным числом
+        schemas.put("age", v.number().positive());
+
+        // Настраиваем схему `MapSchema`
+        // Передаем созданный набор схем в метод shape()
+        schema.shape(schemas);
+
+        // Проверяем объекты
+        Map<String, Object> human1 = new HashMap<>();
+        human1.put("name", "Kolya");
+        human1.put("age", 100);
+        assertThat(schema.isValid(human1)).isTrue();
+
+        Map<String, Object> human2 = new HashMap<>();
+        human2.put("name", "Maya");
+        human2.put("age", null);
+        assertThat(schema.isValid(human2)).isTrue();
+
+        Map<String, Object> human3 = new HashMap<>();
+        human3.put("name", "");
+        human3.put("age", null);
+        assertThat(schema.isValid(human3)).isFalse();
+
+        Map<String, Object> human4 = new HashMap<>();
+        human4.put("name", "Valya");
+        human4.put("age", -5);
+        assertThat(schema.isValid(human4)).isFalse();
+
     }
 }
